6d6e1c03d086cae6e9821c25780965353b722f51
kishorbinwade <kbinwade.1996@gmail.com>
2021-06-12 17:12:01 +0530

    deletion task page updated in task application

--- mySite/task/urls.py
+++ mySite/task/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name="task"
 urlpatterns = [
     path("", views.index,name="index"),
-    path("add/",views.addTask,name="add")
+    path("add/",views.addTask,name="add"),
+    path("delete/",views.deleteTask,name="delete")
    
 ]
--- mySite/task/views.py
+++ mySite/task/views.py
@@ -8,17 +8,24 @@ from django.urls import reverse
 class newTaskForm(forms.Form):
     tasks=forms.CharField(label="New Task")
     priority=forms.IntegerField(label="Priority",min_value=1,max_value=5)
-    delete_tasks=forms.CharField(label="Delete task",blank=True)
+    #delete_tasks=forms.CharField(label="Delete task",blank=True)
     #CHOICES = [('yes', 'Yes'), ('no', 'No')]
     #delete_tasks=forms.CharField(label="Do you want delete task",widget=forms.RadioSelect(choices=CHOICES))
     
-   
+
+    
 '''
     if delete_tasks=='yes':
         delete_task_input=forms.CharField(label="Enter deletion task")
     else:
         None    
 '''
+
+class deleteForm(forms.Form):
+    delete_tasks=forms.CharField(label="Delete task",required=False)
+
+    
+
 def index(request):
     if "task" not in request.session:  
         request.session["task"]=[]              #request.session["task"] used here to not repeat same content to another web page.
@@ -32,11 +39,18 @@ def addTask(request):
         if form.is_valid():
             task_list=request.session["task"]
             Updated_task=form.cleaned_data["tasks"]
-            delete_task1=form.cleaned_data["delete_tasks"]
-            #request.session["task"]+=[Updated_task]
             task_list+=[Updated_task]
-            task_list.remove(delete_task1)
+            #request.session["task"]+=[Updated_task]
+           
             request.session.modified = True    
+
+            
+
+        
+
+                            
+
+
             return HttpResponseRedirect(reverse("task:index")) #to go reverse and check task index page 
         else:
             return render(request,"task/add.html",{
@@ -46,12 +60,40 @@ def addTask(request):
         "form":newTaskForm()
     }
     ) 
-'''      
-def deleteTask(request):
-    if request.method:
-        form=newTaskForm(request.POST)
-        if form.delete_tasks:
-            request.session["task"]-=[form.delete_tasks]
-            return HttpResponseRedirect(reverse,"task:index")
+      
 
-'''
+def deleteTask(request):
+    """
+    if request.method=="POST":
+        form=deleteForm(request.POST)
+        if form.delete_tasks.is_valid:
+            delete_task1=form.cleaned_data["delete_tasks"]
+    
+            if delete_task1 not in  task_list :
+                pass
+            else:    
+                task_list.remove(delete_task1)
+          return HttpResponseRedirect(reverse("task:index")) 
+"""
+    if request.method=="POST":
+        form=deleteForm(request.POST)
+        if form.is_valid():
+            task_list=request.session["task"]
+            delete_task1=form.cleaned_data["delete_tasks"]
+            if delete_task1 not in  task_list :
+                pass
+            else:    
+                task_list.remove(delete_task1)
+           
+            request.session.modified = True    
+
+            return HttpResponseRedirect(reverse("task:index")) #to go reverse and check task index page 
+        else:
+            return render(request,"task/delete.html",{
+                "form":form
+            })    
+    return render(request,"task/delete.html", {
+        "form":deleteForm()
+    }
+    )      
+
